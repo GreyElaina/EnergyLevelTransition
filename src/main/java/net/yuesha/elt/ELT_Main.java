8a4f63a6d19b2145eba7a21c947c2f35cbc25a84
yuesha-yc <kevinwangyichen@gmail.com>
2020-07-11 23:12:35 +0800

    Added a symbol

--- src/main/java/net/yuesha/elt/ELT_Main.java
+++ src/main/java/net/yuesha/elt/ELT_Main.java
@@ -1,31 +1,24 @@
 package net.yuesha.elt;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ELT_Main implements ModInitializer {
+
+	public static final Item ELT_SYMBOL = new Item(new Item.Settings().group(ItemGroup.MISC));
+
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("########################################");
-		System.out.println("########################################");
-		System.out.println("###############          ###############");
-		System.out.println("#############              #############");
-		System.out.println("###############          ###############");
-		System.out.println("##########  #######  #######  ##########");
-		System.out.println("#########   #######  #######   #########");
-		System.out.println("#########   #######  #######   #########");
-		System.out.println("#########   #######  #######   #########");
-		System.out.println("#########   #######  #######   #########");
-		System.out.println("#########   #######  #######   #########");
-		System.out.println("#########   #######  #######   #########");
-		System.out.println("########################################");
-		System.out.println("########## Welcome To ELT ##############");
-		System.out.println("########################################");
-		System.out.println("############ Made by TeamMoeg ##########");
-		System.out.println("########################################");
+		Registry.register(Registry.ITEM, new Identifier("elt", "symbol"), ELT_SYMBOL);
+
 
 	}
 }
