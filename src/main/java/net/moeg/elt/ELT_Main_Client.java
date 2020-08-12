26b55111f913dbeb44cf257b7e4d192142bf19be
yuesha-yc <kevinwangyichen@gmail.com>
2020-08-12 23:10:58 +0800

    Finish Egestion Mechanics `clean codes`

--- src/main/java/net/moeg/elt/ELT_Main_Client.java
+++ src/main/java/net/moeg/elt/ELT_Main_Client.java
@@ -5,7 +5,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import net.moeg.elt.gui.WoodCutterScreen;
 import net.moeg.elt.handlers.ScreenHandlerTypeELT;
-import net.moeg.elt.render.LeavesColorProvider;
+import net.moeg.eltcore.render.LeavesColorProvider;
 
 import static net.moeg.elt.handlers.Handler_Blocks.EXAMPLE_BLOCK;
 
@@ -14,7 +14,6 @@ public class ELT_Main_Client implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ColorProviderRegistry.BLOCK.register(LeavesColorProvider.INSTANCE, EXAMPLE_BLOCK);
-        //ScreenProviderRegistry.INSTANCE.<WoodCutterScreenHandler>registerFactory(ELT_Main.WOOD_CUTTER, (container) -> new WoodCutterScreen(container, MinecraftClient.getInstance().player.inventory, new TranslatableText(ELT_Main.WOOD_CUTTER_TRANSLATION_KEY)));
         ScreenRegistry.register(ScreenHandlerTypeELT.WOOD_CUTTER, WoodCutterScreen::new);
     }
 }
